7b79e2f046df88c41ae8dfdb98025ae26f5f3c5c
rafaelmoresco <rafaelmorescovieira@gmail.com>
2026-03-30 20:32:18 -0300

    orfs: fix flow to do random IO

--- flow/scripts/io_placement_random.tcl
+++ flow/scripts/io_placement_random.tcl
@@ -1,14 +1,15 @@
 source $::env(SCRIPTS_DIR)/load.tcl
-erase_non_stage_variables floorplan
-
-if {![env_var_equals IS_CHIP 1]} {
-  load_design 2_1_floorplan.odb 2_floorplan.sdc
+if {
+  ![env_var_exists_and_non_empty FLOORPLAN_DEF] &&
+  ![env_var_exists_and_non_empty FOOTPRINT] &&
+  ![env_var_exists_and_non_empty FOOTPRINT_TCL]
+} {
+  load_design 2_1_floorplan.odb 2_1_floorplan.sdc
   log_cmd place_pins \
     -hor_layers $::env(IO_PLACER_H) \
     -ver_layers $::env(IO_PLACER_V) \
     -random \
     {*}[env_var_or_empty PLACE_PINS_ARGS]
-  source $::env(SCRIPTS_DIR)/io_placement_util.tcl
   write_db $::env(RESULTS_DIR)/2_2_floorplan_io.odb
 } else {
   log_cmd exec cp $::env(RESULTS_DIR)/2_1_floorplan.odb $::env(RESULTS_DIR)/2_2_floorplan_io.odb
